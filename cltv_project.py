5fe7f2404d83c5be938c9107970590055d40200f
HakanGnes <100923570+HakanGnes@users.noreply.github.com>
2022-12-27 13:25:08 +0300

    Create cltv_project.py

---
+++ cltv_project.py
@@ -0,0 +1,261 @@
+#########################################
+# BG-NBD ve Gamma-Gamma ile CLTV Tahmini
+#########################################
+
+# İş Problemi
+
+# İngiltere merkezli perakende şirketi müşterilerini segmentlere ayırıp bu segmentlere göre pazarlama stratejileri belirlemek istemektedir.
+# Ortak davranışlar sergileyen müşteri segmentleri özelinde pazarlama çalışmaları yapmanın gelir artışı sağlayacağını düşünmektedir.
+# Segmentlere ayırmak için RFM analizi kullanılacaktır.
+
+# Veri Seti Hikayesi
+
+# Online Retail II isimli veri seti İngiltere merkezli bir perakende şirketinin 01/12/2009 - 09/12/2011 tarihleri
+# arasındaki online satış işlemlerini içeriyor. Şirketin ürün kataloğunda hediyelik eşyalar yer almaktadır ve çoğu
+# müşterisinin toptancı olduğu bilgisi mevcuttur.
+
+# Degiskenler
+
+#InvoiceNo = Fatura Numarası ( Eğer bu kod C ile başlıyorsa işlemin iptal edildiğini ifade eder )
+# StockCode = Ürün kodu ( Her bir ürün için eşsiz )
+# Description = Ürün ismi
+# Quantity = Ürün adedi  ( Faturalardaki ürünlerden kaçar tane satıldığı)
+# InvoiceDate = Fatura tarihi
+# UnitPrice = Fatura fiyatı ( Sterlin )
+# CustomerID = Eşsiz müşteri numarası
+# Country = Ülke ismi
+
+
+##########################
+# Gerekli Kütüphane ve Fonksiyonlar
+##########################
+
+# !pip install lifetimes
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from lifetimes.plotting import plot_period_transactions
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+from sklearn.preprocessing import MinMaxScaler
+
+# Veri İşlenmesi
+
+df_ = pd.read_excel("crmAnalytics/datasets/online_retail_II.xlsx", sheet_name="Year 2010-2011")
+df = df_.copy()
+df.head()
+
+# Aykırı değerleri baskılamak için gerekli olan outlier_thresholds ve replace_with_thresholds fonksiyonlarını tanımlayınız
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    # dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+#########################
+# Veri Ön İşleme
+#########################
+df = df[~df["Country"].str.contains("France", na=False)]
+df.dropna(inplace=True)
+df = df[~df["Invoice"].str.contains("C", na=False)]
+df = df[df["Quantity"] > 0]
+df = df[df["Price"] > 0]
+
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+today_date = dt.datetime(2011, 12, 11)
+
+threshold_check = ["Quantity","Price"]
+for col in threshold_check:
+    replace_with_thresholds(df, col)
+
+#########################
+# Lifetime Veri Yapısının Hazırlanması
+#########################
+
+# recency: Son satın alma üzerinden geçen zaman. Haftalık. (kullanıcı özelinde)
+# T: Müşterinin yaşı. Haftalık. (analiz tarihinden ne kadar süre önce ilk satın alma yapılmış)
+# frequency: tekrar eden toplam satın alma sayısı (frequency>1)
+# monetary: satın alma başına ortalama kazanç
+
+
+cltv_df = df.groupby('Customer ID').agg(
+    {'InvoiceDate': [lambda InvoiceDate: (InvoiceDate.max() - InvoiceDate.min()).days,
+                     lambda InvoiceDate: (today_date - InvoiceDate.min()).days],
+     'Invoice': lambda Invoice: Invoice.nunique(),
+     'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+cltv_df.columns = cltv_df.columns.droplevel(0)
+
+cltv_df.columns = ['recency', 'T', 'frequency', 'monetary']
+
+cltv_df["monetary"] = cltv_df["monetary"] / cltv_df["frequency"]
+
+cltv_df.describe().T
+
+cltv_df = cltv_df[(cltv_df['frequency'] > 1)]
+
+cltv_df["recency"] = cltv_df["recency"] / 7
+
+cltv_df["T"] = cltv_df["T"] / 7
+
+##############################################################
+# BG-NBD Modelinin Kurulması
+##############################################################
+
+bgf = BetaGeoFitter(penalizer_coef=0.001)
+
+bgf.fit(cltv_df['frequency'],
+        cltv_df['recency'],
+        cltv_df['T'])
+
+# Modeller
+bgf.predict(4 * 6,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sum()
+
+cltv_df["expected_purc_6_month"] = bgf.predict(4 * 6,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency'],
+                                               cltv_df['T'])
+
+bgf.predict(4,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sum()
+
+cltv_df["expected_purc_1_month"] = bgf.predict(4,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency'],
+                                               cltv_df['T'])
+
+bgf.predict(4 * 12,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sum()
+
+cltv_df["expected_purc_12_month"] = bgf.predict(4 * 12,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency'],
+                                               cltv_df['T'])
+################################################################
+# Tahmin Sonuçlarının Değerlendirilmesi
+################################################################
+
+plot_period_transactions(bgf)
+plt.show(block=True)
+
+##############################################################
+#  GAMMA-GAMMA Modelinin Kurulması
+##############################################################
+
+ggf = GammaGammaFitter(penalizer_coef=0.01)
+
+ggf.fit(cltv_df['frequency'], cltv_df['monetary'])
+
+ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                        cltv_df['monetary']).head(10)
+
+ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                        cltv_df['monetary']).sort_values(ascending=False).head(10)
+
+cltv_df["expected_average_profit"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                             cltv_df['monetary'])
+cltv_df.sort_values("expected_average_profit", ascending=False).head(10)
+
+cltv = ggf.customer_lifetime_value(bgf,
+                                   cltv_df['frequency'],
+                                   cltv_df['recency'],
+                                   cltv_df['T'],
+                                   cltv_df['monetary'],
+                                   time=6, # 6 aylık
+                                   freq="W",  # T'nin frekans bilgisi.
+                                   discount_rate=0.01)
+
+cltv.head()
+
+cltv = cltv.reset_index()
+
+cltv_final = cltv_df.merge(cltv, on="Customer ID", how="left")
+cltv_final.sort_values(by="clv", ascending=False).head(10)
+
+
+
+
+# 2010-2011 UK müşterileri için 1 aylık ve 12 aylık CLTV hesaplayınız.
+# 1 aylık CLTV'de en yüksek olan 10 kişi ile 12 aylık'taki en yüksek 10 kişiyi analiz ediniz.
+
+
+cltv = ggf.customer_lifetime_value(bgf,
+                                   cltv_df['frequency'],
+                                   cltv_df['recency'],
+                                   cltv_df['T'],
+                                   cltv_df['monetary'],
+                                   time=1,  # 1 aylık
+                                   freq="W",  # T'nin frekans bilgisi.
+                                   discount_rate=0.01)
+
+cltv.head()
+
+cltv = cltv.reset_index()
+
+cltv_final = cltv_df.merge(cltv, on="Customer ID", how="left")
+cltv_final.sort_values(by="clv", ascending=False).head(10)
+
+cltv = ggf.customer_lifetime_value(bgf,
+                                   cltv_df['frequency'],
+                                   cltv_df['recency'],
+                                   cltv_df['T'],
+                                   cltv_df['monetary'],
+                                   time=12,  # 12 aylık
+                                   freq="W",  # T'nin frekans bilgisi.
+                                   discount_rate=0.01)
+
+cltv.head()
+
+cltv_final = cltv.reset_index()
+
+cltv_final = cltv_df.merge(cltv, on="Customer ID", how="left")
+cltv_final.sort_values(by="clv", ascending=False).head(10)
+
+##############################################################
+# 5. CLTV'ye Göre Segmentlerin Oluşturulması
+##############################################################
+
+cltv_final
+
+cltv_final["segment"] = pd.qcut(cltv_final["clv"], 4, labels=["D", "C", "B", "A"])
+
+cltv_final.sort_values(by="clv", ascending=False).head(50)
+
+cltv_final.groupby("segment").agg(
+    {"count", "mean", "sum"})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
